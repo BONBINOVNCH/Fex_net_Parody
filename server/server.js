f553aac79d83b6c3b51ed68dfaf45a38a0d5d479
MaksymZaluskiy <zmax1301@gmail.com>
2025-12-27 16:05:24 +0200

    config connect to mongodb Atlas

--- server/server.js
+++ server/server.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
+const mongoose = require("mongoose");
+const { create } = require("domain");
 require("dotenv").config();
+
+//config express
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+//config db
+
+const URL_DB = process.env.URL_DB;
+mongoose
+    .connect(URL_DB)
+    .then(() => {
+        console.log("Connected to the database");
+    })
+    .catch((err) => {
+        console.error("Database connection error:", err);
+    });
+
+const FileSchema = new mongoose.Schema({
+    filrname: String,
+    path: String,
+    code: {
+        type: String,
+        unique: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const File = mongoose.model("File", FileSchema);
+
+//config storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads/");
